4530c4cf778db9420196c88cc8f5bb41ff5b52df
Leandro Estevez <leoest.dev@gmail.com>
2023-10-31 21:12:59 -0500

    started files

---
+++ src/main/java/com/github/LeandroEstevez/yourbudgetappapi/YourbudgetappapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.LeandroEstevez.yourbudgetappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourbudgetappapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourbudgetappapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/LeandroEstevez/yourbudgetappapi/YourbudgetappapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LeandroEstevez.yourbudgetappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourbudgetappapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
